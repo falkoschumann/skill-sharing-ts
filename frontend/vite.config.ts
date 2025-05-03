8c3e63a2d55812ffcfec971aa8d2348db1bf9e95
Falko Schumann <falko.schumann@muspellheim.de>
2025-05-03 19:53:34 +0200

    feat: use SWC in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,11 @@
 
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ tsDecorators: true })],
   server: {
     port: process.env.DEV_PORT ? parseInt(process.env.DEV_PORT) : 3000,
     proxy: {
