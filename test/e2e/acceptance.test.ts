0ed3bf8d00bdf75146e6b9dd811eaacd3266af5d
Falko Schumann <falko.schumann@muspellheim.de>
2025-05-14 21:45:55 +0200

    test: increase timeout for e2e test

--- test/e2e/acceptance.test.ts
+++ test/e2e/acceptance.test.ts
@@ -14,13 +14,17 @@ describe("User acceptance test", () => {
     await sut.start();
   });
 
-  it("Submit and comment a talk", async () => {
-    await sut.gotoSubmission();
-    await sut.setViewport(1024, 768);
-    await sut.saveScreenshot("01-app-started");
+  it(
+    "Submit and comment a talk",
+    async () => {
+      await sut.gotoSubmission();
+      await sut.setViewport(1024, 768);
+      await sut.saveScreenshot("01-app-started");
 
-    expect(true).equals(true);
-  });
+      expect(true).equals(true);
+    },
+    { timeout: 60000 },
+  );
 
   afterEach(async () => {
     await sut.stop();
